f461db0c60a729dfd57fd3196bbcddc834287f8b
Nikhil <nikhildixit2125@gmail.com>
2023-05-24 17:17:26 +0530

    Project 6

---
+++ Projects/3D Rotating Image Gallery/index.js
@@ -0,0 +1,27 @@
+const imageContainerEl = document.querySelector(".image-container");
+const prevEl = document.getElementById("prev");
+const nextEl = document.getElementById("next");
+
+let x = 0;
+let timer;
+prevEl.addEventListener("click",()=>{
+    x = x+45;
+    clearTimeout(timer);
+    updateGallary();
+})
+
+nextEl.addEventListener("click",()=>{
+    x = x-45;
+    clearTimeout(timer);
+    updateGallary();
+})
+
+function updateGallary(){
+    imageContainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+    timer = setTimeout(()=>{
+        x = x-45;
+        updateGallary();
+    }, 3000)
+}
+
+updateGallary();
